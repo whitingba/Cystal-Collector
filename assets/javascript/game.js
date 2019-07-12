179a06bc5c0465edd1965a90ae4c5fe4730f4fdb
Barbara Whiting <whitingba@gmail.com>
2019-07-12 08:08:43 -0600

    pseduocode for my javascript file

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+//variables:
+//compRandomChoices
+//compRandomNum
+//winsCounter
+//LossCounter
+//crystalRandonNum (will need one for each crystal)
+//crystalGuessArray
+//
+
+//Upon start of game a random number will be generated and attached to #randonNumber.
+//This is the randon number that the user will try to match by clicking the crystals.
+//Also at the start of the game when one of the crystals is 'clicked' a random number will be genarated and added
+//to the players score. 
+//It will keep this same random number throughout that game play.
+//The user will keep clicking the crystals until it matches the randomNumber without going over
+//If the user matches the randonNumber => 'You win!' will show on the screen and win score increses by 1
+//game is reset upon on click of a crystal
+//If their score is greater than the randonNumber => 'You lose, try again!' and losses will increase by 1
+//game is reset upon on click of a crystal
