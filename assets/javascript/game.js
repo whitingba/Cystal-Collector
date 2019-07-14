935817e220b3ced243a09d1d60bc726eaf485b77
Barbara Whiting <whitingba@gmail.com>
2019-07-14 13:00:47 -0600

    fixed program bugs. Just need to correct game score & generating the random number when game is reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,12 +11,14 @@ let crystalRandonNumThree = Math.floor(Math.random() * 12) + 1;
 let crystalRandonNumFour = Math.floor(Math.random() * 12) + 1;
 //array for the randum number for each crystal to go to
 let crystalGuessArray = [];
+//set game state
+let gameOver = true;
+
+//TODO:- double check game functions as it should
 
 
-
-
-
-//check that the user's score is less than the random number to meet and if it is the numbers will continue to the array 
+//check that the user's score is less than the random number generated and if it is less  
+//then the numbers will continue to the array 
 let checkMyTotal = function (parameterOne) {
     if (calculateTotal() < compRandomNum) {
         //then the random number will be pushed to the array
@@ -33,21 +35,7 @@ let calculateTotal = function () {
     return total;
 };
 
-//reset game for new play. Game message banner reads: "You win/lose, click here to play again"
-let resetGame = (function () {
-    //on click of message banner
-    $("#gameOutcome").click(function () {
-        //new random number is generated
-        //FIXME: not generating random number upon click to reset game
-        $("#randomNumber").html(compRandomNum);
-        //message banner changes with what to do next
-        $("#gameOutcome").text("Click the crystals to match the number above");
-        //the players score resets message
-        $("#playerScore").text("Your score");
-        //guess array clears
-        crystalGuessArray = [];
-    });
-});
+
 
 //function created to handle what happens when the user wins or loses
 let winOrLose = function () {
@@ -59,6 +47,8 @@ let winOrLose = function () {
         winsCounter = winsCounter + 1;
         //display wins count on screen
         $("#wins").text("Wins: " + winsCounter);
+        //state of the game set to true
+        gameOver = true;
         //call reset game function
         resetGame();
     }
@@ -67,22 +57,41 @@ let winOrLose = function () {
         $("#gameOutcome").text("You lose, click here to play again!");
         lossCounter = lossCounter + 1;
         $("#losses").text("Losses: " + lossCounter);
+        //state of the game set to true
+        gameOver = true;
         //call reset game function
         resetGame();
 
     }
 };
 
+
+//reset game for new play. Game message banner reads: "You win/lose, click here to play again"
+let resetGame = (function () {
+    //if state of game is true, then
+    if (gameOver == true) {
+        //new random number is generated
+        //FIXME: not generating random number upon click to reset game
+        $("#randomNumber").html(compRandomNum);
+        //the players score resets message to "Your Score"
+        $("#playerScore").text("Your score");
+        //guess array clears to reset game
+        crystalGuessArray = [];
+        //state of game is now set to false 
+        gameOver = false;
+    }
+
+});
+
 //FIXME: only count one loss or one win. remove the ability to count multiple wins or losses with additional
 //clicks to the crystals
 
+// $(document).ready(function () {
 
+//to start the game the game message banner is clicked 
+$("#gameOutcome").click(function () {
 
-$(document).ready(function () {
-
-    //to start the game the game message banner is clicked 
-    $("#gameOutcome").click(function () {
-
+    if (gameOver === false) {
 
         // random number of 'cystals' is generated and placed in the 'Crystals to collect' box
         $("#randomNumber").html(compRandomNum);
@@ -90,49 +99,53 @@ $(document).ready(function () {
         //Message banner changes with new instructions:
         $("#gameOutcome").text("Click the crystals to match the number above");
 
-        //TODO:- appears I have removed the issue with clicking on gameOutcome banner and generating a new
-        //random compRandomNum on each click. Just double check this is still working before submission
 
-    });
-
-    //When one of the crystals is 'clicked' a random number (1-12) will be genarated
-    $('#GemOne').click(function () {
-
-        checkMyTotal(crystalRandonNumOne);
-        //and as the crystals are clicked the #playerScore will increase
-        $("#playerScore").text(calculateTotal());
-        //call win/lose function
-        winOrLose();
-    });
-    //click event for GemTwo same functions as GemOne
-    $('#GemTwo').click(function () {
-
-        checkMyTotal(crystalRandonNumTwo);
-
-        $("#playerScore").text(calculateTotal());
-
-        winOrLose();
-    });
-    //click event for GemThree
-    $('#GemThree').click(function () {
-
-        checkMyTotal(crystalRandonNumThree);
-
-        $("#playerScore").text(calculateTotal());
-
-        winOrLose();
-    });
-    //click event for GemFour
-    $('#GemFour').click(function () {
-
-        checkMyTotal(crystalRandonNumFour);
-
-        $("#playerScore").text(calculateTotal());
-
-        winOrLose();
-    });
-
-
+    }
+    else {
+        resetGame();
+        //       message banner changes with what to do next
+        $("#gameOutcome").text("Click the crystals to match the number above");
+    }
 });
+
+//When one of the crystals is 'clicked' a random number (1-12) will be genarated
+$('#GemOne').click(function () {
+
+    checkMyTotal(crystalRandonNumOne);
+    //and as the crystals are clicked the #playerScore will increase
+    $("#playerScore").text(calculateTotal());
+    //call win/lose function
+    winOrLose();
+});
+//click event for GemTwo same functions as GemOne
+$('#GemTwo').click(function () {
+
+    checkMyTotal(crystalRandonNumTwo);
+
+    $("#playerScore").text(calculateTotal());
+
+    winOrLose();
+});
+//click event for GemThree
+$('#GemThree').click(function () {
+
+    checkMyTotal(crystalRandonNumThree);
+
+    $("#playerScore").text(calculateTotal());
+
+    winOrLose();
+});
+//click event for GemFour
+$('#GemFour').click(function () {
+
+    checkMyTotal(crystalRandonNumFour);
+
+    $("#playerScore").text(calculateTotal());
+
+    winOrLose();
+});
+
+
+// });
 
 
