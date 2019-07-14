5110d1116db5045878154b8b2684bf1486749e00
Barbara Whiting <whitingba@gmail.com>
2019-07-14 11:30:22 -0600

    updates to game. Reset game is now functioning with exception of generating the random number. Need to fix wins loss counter.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,25 @@
-
+//variables
+//wins or losses counter:
 let winsCounter = 0;
 let lossCounter = 0;
-let compRandomNum;
+//random number generated for number of crystals to get
+let compRandomNum = Math.floor(Math.random() * 102) + 19;
+//randum number generated for each of the four cystals
 let crystalRandonNumOne = Math.floor(Math.random() * 12) + 1;
 let crystalRandonNumTwo = Math.floor(Math.random() * 12) + 1;
 let crystalRandonNumThree = Math.floor(Math.random() * 12) + 1;
 let crystalRandonNumFour = Math.floor(Math.random() * 12) + 1;
+//array for the randum number for each crystal to go to
 let crystalGuessArray = [];
-let gameOver = true;
+
+
+
 
 
 //check that the user's score is less than the random number to meet and if it is the numbers will continue to the array 
 let checkMyTotal = function (parameterOne) {
     if (calculateTotal() < compRandomNum) {
-        //that random number will be pushed to the array
+        //then the random number will be pushed to the array
         crystalGuessArray.push(parameterOne);
     }
 };
@@ -27,69 +33,78 @@ let calculateTotal = function () {
     return total;
 };
 
+//reset game for new play. Game message banner reads: "You win/lose, click here to play again"
+let resetGame = (function () {
+    //on click of message banner
+    $("#gameOutcome").click(function () {
+        //new random number is generated
+        //FIXME: not generating random number upon click to reset game
+        $("#randomNumber").html(compRandomNum);
+        //message banner changes with what to do next
+        $("#gameOutcome").text("Click the crystals to match the number above");
+        //the players score resets message
+        $("#playerScore").text("Your score");
+        //guess array clears
+        crystalGuessArray = [];
+    });
+});
+
 //function created to handle what happens when the user wins or loses
 let winOrLose = function () {
     //The user will keep clicking the crystals until it matches the randomNumber without going over
     //If the user matches the randonNumber => 'You win!' will show on the screen 
     if (calculateTotal() === compRandomNum) {
-        $("#gameOutcome").text("You are a winner!");
+        $("#gameOutcome").text("You win, click here to play again!");
         //winsCounter increases by 1
         winsCounter = winsCounter + 1;
         //display wins count on screen
         $("#wins").text("Wins: " + winsCounter);
+        //call reset game function
+        resetGame();
     }
     //If their score is greater than the randonNumber => 'You lose, try again!' and losses will increase by 1
-
     if (calculateTotal() > compRandomNum) {
-        $("#gameOutcome").text("You lose, click here to play again");
+        $("#gameOutcome").text("You lose, click here to play again!");
         lossCounter = lossCounter + 1;
         $("#losses").text("Losses: " + lossCounter);
+        //call reset game function
+        resetGame();
 
     }
 };
-//TODO: only count one loss or one win. remove the ability to count multiple wins or losses with additional
+
+//FIXME: only count one loss or one win. remove the ability to count multiple wins or losses with additional
 //clicks to the crystals
-
-
-//TODO: create a function as to what happens when the gameOutcome banner is clicked to restart the game
-
 
 
 
 $(document).ready(function () {
 
-
-
+    //to start the game the game message banner is clicked 
     $("#gameOutcome").click(function () {
 
-        //Upon start of game a random number (19 - 120) will be generated and attached to #randonNumber.
-        //This is the randon number that the user will try to match by clicking the crystals.
-        //To start game they will click any key
 
-        compRandomNum = Math.floor(Math.random() * 102) + 19;
+        // random number of 'cystals' is generated and placed in the 'Crystals to collect' box
         $("#randomNumber").html(compRandomNum);
 
-        //have message: 'Click mouse to begin' change to a new message
-        $("#gameOutcome").text("Now click on crystals to add to your score")
+        //Message banner changes with new instructions:
+        $("#gameOutcome").text("Click the crystals to match the number above");
 
-        //TODO:- remove random number generating upon multiple clicks to #gameOutcome
-
-
+        //TODO:- appears I have removed the issue with clicking on gameOutcome banner and generating a new
+        //random compRandomNum on each click. Just double check this is still working before submission
 
     });
-
 
     //When one of the crystals is 'clicked' a random number (1-12) will be genarated
     $('#GemOne').click(function () {
 
-
         checkMyTotal(crystalRandonNumOne);
         //and as the crystals are clicked the #playerScore will increase
         $("#playerScore").text(calculateTotal());
-
+        //call win/lose function
         winOrLose();
     });
-    //click event for GemTwo
+    //click event for GemTwo same functions as GemOne
     $('#GemTwo').click(function () {
 
         checkMyTotal(crystalRandonNumTwo);
@@ -116,9 +131,6 @@ $(document).ready(function () {
 
         winOrLose();
     });
-
-
-
 
 
 });
