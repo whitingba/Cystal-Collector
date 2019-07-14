b8f9ff9e9e5b9b4d874e20c980573da7dde43b18
Barbara Whiting <whitingba@gmail.com>
2019-07-13 19:00:36 -0600

    fixed jquery script, now pulling in random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-let compRandomNum = Math.floor(Math.random() * 102) + 19;
+
 let winsCounter = 0;
 let lossCounter = 0;
 let crystalRandonNumOne = Math.floor(Math.random() * 12) + 1;
@@ -25,13 +25,13 @@ $(document).ready(function () {
 
 
 
-    console.log(compRandomNum);
-
     $(document).keyup(function () {
 
         //Upon start of game a random number (19 - 120) will be generated and attached to #randonNumber.
         //This is the randon number that the user will try to match by clicking the crystals.
+        //To start game they will click any key
 
+        let compRandomNum = Math.floor(Math.random() * 102) + 19;
         $("#randomNumber").html(compRandomNum);
 
 
@@ -45,14 +45,14 @@ $(document).ready(function () {
 
         //When one of the crystals is 'clicked' a random number (1-12) will be genarated and added
         //to the players score. 
-        $('#GemOne').on('click', function () {
+        // $('#GemOne').on('click', function () {
 
 
-            //         //crystal random number will display in #playerScore on click of crystal
-            //         //and as the crystals are clicked the #playerScore will increase
+        //         //crystal random number will display in #playerScore on click of crystal
+        //         //and as the crystals are clicked the #playerScore will increase
 
 
-
-        });
 
     });
+
+});
